2abed99b1db17d205d972fe9b87986f8cfadd3b1
Carolina Barcelos <68038694+barceloscarolina@users.noreply.github.com>
2026-09-11 01:05:07 +0000

    livros e autores

---
+++ backend/src/main/java/cbarcelos/backend/ExemploController.java
@@ -0,0 +1,49 @@
+package cbarcelos.backend;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/biblioteca")public class ExemploController {
+
+    private List<Livro> livros;
+
+    public ExemploController() {
+        livros = new ArrayList<>();
+
+        livros.add(new Livro(110, "Aprendendo Java", "Maria da Silva", 2015));
+        livros.add(new Livro(120, "Spring-Boot", "Jose de Souza", 2020));
+        livros.add(new Livro(130, "Principios SOLID", "Pedro da Silva", 2023));
+        livros.add(new Livro(140, "Padroes de Projeto", "Joana Moura", 2023));
+        livros.add(new Livro(150, "Teste Unitario", "Pedro da Silva", 2024)); 
+    }
+
+    @GetMapping("/")
+    public String getMensagemInicial() {
+        return "Aplicacao Spring-Boot funcionando!";
+    }
+
+    @GetMapping("/livros")
+    public List<Livro> getLivros() {
+        return livros;
+    }
+    
+    @GetMapping("/titulos")
+    public List<String> getTitulos() {
+        return livros.stream()
+               .map(livro->livro.getTitulo())
+               .toList();
+    }
+
+    @GetMapping("/autores")
+    public List<String> getListaAutores() {
+        return livros.stream()
+                .map(l -> l.getAutor())
+                .distinct()
+                .toList();
+    }
+
+}
